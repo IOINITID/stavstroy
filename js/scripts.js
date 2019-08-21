71b73ca82f47898accde8d99ba48a9d26e026a85
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-08-21 16:52:22 +0300

    Added tablet adaptation for site

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-var slideout = new Slideout({
-  'panel': document.getElementById('panel'),
-  'menu': document.getElementById('menu'),
-  'padding': 256,
-  'tolerance': 70
-});
+// var slideout = new Slideout({
+//   'panel': document.getElementById('panel'),
+//   'menu': document.getElementById('menu'),
+//   'padding': 256,
+//   'tolerance': 70
+// });
 
-slideout.disableTouch();
+// slideout.disableTouch();
 
-// Toggle button
-document.querySelector('.toggle-button').addEventListener('click', function() {
-  slideout.toggle();
-});
+// document.querySelector('.toggle-button').addEventListener('click', function() {
+//   slideout.toggle();
+// });
